15b986fd7006bd823fdb8aecb80a9e93b4f748e0
Alexy Araujo <96698163+a-gva@users.noreply.github.com>
2022-02-24 22:29:44 -0300

    Add files via upload

---
+++ src/context/github/GithubContext.jsx
@@ -0,0 +1,93 @@
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
+
+// Create Context
+const GithubContext = createContext()
+
+// Process .env data
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+// Provider Function
+export const GithubProvider = ({ children }) => {
+
+    // Users initial state
+    const initialState = {
+        users: [],
+        user: {},
+        loading: false
+    }
+
+    const [state, dispatch] = useReducer(githubReducer, initialState)
+
+    // Get search users (testing purposes)
+    const searchUsers = async (text) => {
+
+        setLoading()
+
+        const params = new URLSearchParams({
+            q: text
+        })
+
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        })
+
+        const { items } = await response.json()
+
+        dispatch({
+            type: 'GET_USERS',
+            payload: items
+        })
+    }
+
+    // Get single user
+    const getUser = async (login) => {
+
+        setLoading()
+
+        // https://api.github.com/search/users/bradtraversy
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        })
+
+        if (response.status === 404) {
+            window.location = '/notfound'
+        } else {
+            const data = await response.json()
+
+            dispatch({
+                type: 'GET_USER',
+                payload: data
+            })
+        }
+
+    }
+
+    // Clear users from state
+    const clearUsers = () => dispatch({ type: 'CLEAR_USERS' })
+
+    // Set Loading
+    const setLoading = () => dispatch({ type: 'SET_LOADING' })
+
+    return (
+        <GithubContext.Provider
+            value={{
+                users: state.users,
+                loading: state.loading,
+                user: state.user,
+                searchUsers,
+                clearUsers,
+                getUser
+            }}>
+            {children}
+        </GithubContext.Provider>
+    )
+}
+
+export default GithubContext
